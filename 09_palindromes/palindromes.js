36b904ee82aa77c35554e08cca0c97e08dcddcf0
arturop <arturopstem@gmail.com>
2023-02-25 20:58:27 -0500

    Solution to Fundamentals Part 5 exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strArray = str.match(/\w/g);
+  const cleanStr = strArray.join("").toLowerCase();
+  const reverseStr = strArray.reverse().join("").toLowerCase();
+  return cleanStr === reverseStr ? true : false;
+  
+  //   const strArray = str.match(/\w/g);
+  //   const index = Math.floor(strArray.length / 2);
 
+  //   const leftArray = strArray.slice(0, index);
+  //   strArray.reverse();
+  //   const rightArray = strArray.slice(0, index);
+
+  //   const leftStr = leftArray.join("").toLowerCase();
+  //   const rightStr = rightArray.join("").toLowerCase();
+
+  //   return leftStr === rightStr ? true : false;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const thisYear = new Date().getFullYear();
+  people.sort((x, y) => {
+    const xAge = (x.yearOfDeath || thisYear) - x.yearOfBirth;
+    const yAge = (y.yearOfDeath || thisYear) - y.yearOfBirth;
+    return yAge - xAge;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
